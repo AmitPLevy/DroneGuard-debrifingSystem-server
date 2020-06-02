80a43f42e96242bdbeeffb05c4645e3dbcb72b94
RanST <ranshem@live.com>
2020-06-02 13:00:54 +0300

    consts fix

--- consts.js
+++ consts.js
@@ -1,5 +1,5 @@
 module.exports = {
-    URL: "mongodb+srv://<username>:<password>@droneguard-plrg7.mongodb.net/test?retryWrites=true&w=majority",
+    URL: "mongodb+srv://<username>:<password>@droneguard-plrg7.mongodb.net/DroneGuard?retryWrites=true&w=majority",
     DB_USER: "Admin2",
     DB_PASS: "kDerDntBY87AU8MU"
 }
